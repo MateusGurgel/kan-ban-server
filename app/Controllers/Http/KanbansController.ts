18dc77fa683bc0da11a457c698f1f4f2077878d5
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-04-01 19:38:18 -0300

    Kanban store route completed

--- app/Controllers/Http/KanbansController.ts
+++ app/Controllers/Http/KanbansController.ts
@@ -1,6 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Kanban from 'App/Models/Kanban'
+import CreateKanbanValidator from 'App/Validators/CreateKanbanValidator'
 
 export default class KanbansController {
   public async show({}: HttpContextContract) {}
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response }: HttpContextContract) {
+    try {
+      const payload = await request.validate(CreateKanbanValidator)
+
+      const kanban = await Kanban.create({ name: payload.name })
+
+      return response.created(kanban)
+    } catch (error) {
+      return response.badRequest(error)
+    }
+  }
 }
