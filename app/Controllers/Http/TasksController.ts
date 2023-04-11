a37e644a4e48719c26389347f83a93652d5aaa71
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-04-11 18:03:44 -0300

    index field added

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -4,6 +4,7 @@ import Task from 'App/Models/Task'
 import ApiResponse from 'App/Services/ApiResponse'
 import CreateTaskValidator from 'App/Validators/CreateTaskValidator'
 import EditTaskValidator from 'App/Validators/EditTaskValidator'
+import EditTasksValidator from 'App/Validators/EditTasks'
 
 export default class TasksController {
   public async store({ response, request, bouncer, params }: HttpContextContract) {
@@ -17,9 +18,8 @@ export default class TasksController {
     await bouncer.authorize('accessTask', kanban)
 
     const payload = await request.validate(CreateTaskValidator)
-    const task = await kanban.related('tasks').create({
-      content: payload.content,
-    })
+
+    const task = await kanban.related('tasks').create(payload)
 
     return response.created(task)
   }
@@ -45,6 +45,31 @@ export default class TasksController {
     task.merge(payload).save()
 
     return response.created(task)
+  }
+
+  public async updateMany({ response, request, bouncer, params }: HttpContextContract) {
+    const kanbanId = params.kanban
+    const kanban = await Kanban.find(kanbanId)
+
+    if (!kanban) {
+      return ApiResponse.error(response, 404, [{ message: 'kanban not found!' }])
+    }
+
+    await bouncer.authorize('accessTask', kanban)
+
+    const payload = await request.validate(EditTasksValidator)
+
+    payload.tasks.map(async (task) => {
+      const originalTask = await Task.find(task.id)
+
+      if (!originalTask) {
+        return ApiResponse.error(response, 404, [{ message: `task ${task.id} not found!` }])
+      }
+
+      originalTask.merge(task).save()
+    })
+
+    return response.created(payload)
   }
 
   public async index({ response, bouncer, params }: HttpContextContract) {
